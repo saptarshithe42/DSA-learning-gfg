de6e0a0efa1918c4d8ec63ae1d548e114a5ae7b1
Saptarshi Mondal <saptarshi.the.42@gmail.com>
2022-03-28 20:32:19 +0530

    ..

--- DSA Basics/Heap/heap.cpp
+++ DSA Basics/Heap/heap.cpp
@@ -39,7 +39,7 @@ public:
         size++;
         arr[size - 1] = x;
 
-        for (int i = size - 1; (i != 0) && (arr[parent(i)] > arr[i]))
+        for (int i = size - 1; (i != 0) && (arr[parent(i)] > arr[i]);)
         {
             swap(arr[parent(i)], arr[i]);
             i = parent(i);
