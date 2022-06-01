cdb4f194826b7ba5796fd238ac5e3f89b1566ca5
Saptarshi Mondal <saptarshi.the.42@gmail.com>
2022-06-01 17:07:01 +0530

    ..

---
+++ DSA/Searching/2_1_index_of_last_occurence_in_sorted_array.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+
+// time comp : O(logn)
+int index_of_first_occ(int arr[], int n, int x)
+{
+    int low = 0, high = n-1;
+
+    while(low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if(arr[mid] > x)
+            high = mid - 1;
+        else if (arr[mid] < x)
+            low = mid + 1;
+        else
+        {
+            if(mid == (n-1) || arr[mid + 1] != arr[mid])
+                return mid;
+            else
+                low =  mid + 1;
+        }
+    }
+
+
+
+    return -1;
+}
+
+
+int main()
+{
+    int arr[] = {1,10,10,10,20,20,40};
+    int n = 7;
+    int x = 20;
+
+    cout << index_of_first_occ(arr, n, x) << endl;
+
+
+
+
+    
+    return 0;
+}
