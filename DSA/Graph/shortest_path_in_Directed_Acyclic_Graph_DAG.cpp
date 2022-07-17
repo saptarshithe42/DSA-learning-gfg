42a7d91b2fbb6bf436f7e06a963219254490a295
Saptarshi Mondal <saptarshi.the.42@gmail.com>
2022-07-17 13:38:21 +0530

    ..

--- DSA/Graph/shortest_path_in_Directed_Acyclic_Graph_DAG.cpp
+++ DSA/Graph/shortest_path_in_Directed_Acyclic_Graph_DAG.cpp
@@ -1,109 +1,147 @@
-#include <bits/stdc++.h> 
-#define INF INT_MAX 
-using namespace std; 
 
-class AdjListNode 
-{ 
-	int v; 
-	int weight; 
-public: 
-	AdjListNode(int _v, int _w) { v = _v; weight = _w;} 
-	int getV()	 { return v; } 
-	int getWeight() { return weight; } 
-}; 
+// C++ program to find single source shortest
+// paths for Directed Acyclic Graphs
+#include<iostream>
+#include <bits/stdc++.h>
+#define INF INT_MAX
+using namespace std;
 
-class Graph 
-{ 
-	int V;
+// Graph is represented using adjacency list. Every node
+// of adjacency list contains vertex number of the vertex
+// to which edge connects. It also 
+// contains weight of the edge
+class AdjListNode
+{
+    int v;
+    int weight;
+public:
+    AdjListNode(int _v, int _w)  { v = _v;  weight = _w;}
+    int getV()       {  return v;  }
+    int getWeight()  {  return weight; }
+};
 
-	list<AdjListNode> *adj; 
+// Class to represent a graph using adjacency
+// list representation
+class Graph
+{
+    int V;    // No. of vertices'
 
-	void topologicalSortUtil(int v, bool visited[], stack<int> &Stack); 
-public: 
-	Graph(int V); 
- 
-	void addEdge(int u, int v, int weight); 
- 
-	void shortestPath(int s); 
-}; 
+    // Pointer to an array containing adjacency lists
+    list<AdjListNode> *adj;
 
-Graph::Graph(int V) 
-{ 
-	this->V = V; 
-	adj = new list<AdjListNode>[V]; 
-} 
+    // A function used by shortestPath
+    void topologicalSortUtil(int v, bool visited[], stack<int> &Stack);
+public:
+    Graph(int V);   // Constructor
 
-void Graph::addEdge(int u, int v, int weight) 
-{ 
-	AdjListNode node(v, weight); 
-	adj[u].push_back(node);  
-} 
+    // function to add an edge to graph
+    void addEdge(int u, int v, int weight);
 
-void Graph::topologicalSortUtil(int v, bool visited[], stack<int> &Stack) 
-{ 
-	
-	visited[v] = true; 
+    // Finds shortest paths from given source vertex
+    void shortestPath(int s);
+};
 
-	list<AdjListNode>::iterator i; 
-	for (i = adj[v].begin(); i != adj[v].end(); ++i) 
-	{ 
-		AdjListNode node = *i; 
-		if (!visited[node.getV()]) 
-			topologicalSortUtil(node.getV(), visited, Stack); 
-	} 
+Graph::Graph(int V)
+{
+    this->V = V;
+    adj = new list<AdjListNode>[V];
+}
 
-	Stack.push(v); 
-} 
+void Graph::addEdge(int u, int v, int weight)
+{
+    AdjListNode node(v, weight);
+    adj[u].push_back(node); // Add v to u's list
+}
 
-void Graph::shortestPath(int s) 
-{ 
-	stack<int> Stack; 
-	int dist[V]; 
+// A recursive function used by shortestPath. 
+// See below link for details 
+// https://www.cdn.geeksforgeeks.org/topological-sorting/
+void Graph::topologicalSortUtil(int v, bool visited[], stack<int> &Stack)
+{
+    // Mark the current node as visited
+    visited[v] = true;
 
-	bool *visited = new bool[V]; 
-	for (int i = 0; i < V; i++) 
-		visited[i] = false; 
- 
-	for (int i = 0; i < V; i++) 
-		if (visited[i] == false) 
-			topologicalSortUtil(i, visited, Stack); 
+    // Recur for all the vertices adjacent to this vertex
+    list<AdjListNode>::iterator i;
+    for (i = adj[v].begin(); i != adj[v].end(); ++i)
+    {
+        AdjListNode node = *i;
+        if (!visited[node.getV()])
+            topologicalSortUtil(node.getV(), visited, Stack);
+    }
 
-	for (int i = 0; i < V; i++) 
-		dist[i] = INF; 
-	dist[s] = 0; 
+    // Push current vertex to stack which stores topological sort
+    Stack.push(v);
+}
 
-	while (Stack.empty() == false) 
-	{  
-		int u = Stack.top(); 
-		Stack.pop(); 
- 
-		list<AdjListNode>::iterator i; 
-		if (dist[u] != INF) 
-		{ 
-		for (i = adj[u].begin(); i != adj[u].end(); ++i) 
-			if (dist[i->getV()] > dist[u] + i->getWeight()) 
-				dist[i->getV()] = dist[u] + i->getWeight(); 
-		} 
-	} 
+// The function to find shortest paths from given vertex.
+// It uses recursive topologicalSortUtil() to get topological 
+// sorting of given graph.
+void Graph::shortestPath(int s)
+{
+    stack<int> Stack;
+    int dist[V];
 
-	for (int i = 0; i < V; i++) 
-		(dist[i] == INF)? cout << "INF ": cout << dist[i] << " "; 
-} 
+    // Mark all the vertices as not visited
+    bool *visited = new bool[V];
+    for (int i = 0; i < V; i++)
+        visited[i] = false;
 
-int main() 
-{ 
-	Graph g(6); 
-	g.addEdge(0, 1, 2); 
-	g.addEdge(0, 4, 1); 
-	g.addEdge(1, 2, 3); 
-	g.addEdge(4, 2, 2); 
-	g.addEdge(4, 5, 4); 
-	g.addEdge(2, 3, 6); 
-	g.addEdge(5, 3, 1);
+    // Call the recursive helper function to store
+    // Topological Sort starting from all vertices 
+    // one by one
+    for (int i = 0; i < V; i++)
+        if (visited[i] == false)
+            topologicalSortUtil(i, visited, Stack);
 
-	int s = 0; 
-	cout << "Following are shortest distances from source " << s <<" \n"; 
-	g.shortestPath(s); 
+    // Initialize distances to all vertices as
+    // infinite and distance to source as 0
+    for (int i = 0; i < V; i++)
+        dist[i] = INF;
+    dist[s] = 0;
 
-	return 0; 
-} 
+    // Process vertices in topological order
+    while (Stack.empty() == false)
+    {
+        // Get the next vertex from topological order
+        int u = Stack.top();
+        Stack.pop();
+
+        // Update distances of all adjacent vertices
+        list<AdjListNode>::iterator i;
+        if (dist[u] != INF)
+        {
+          for (i = adj[u].begin(); i != adj[u].end(); ++i)
+             if (dist[i->getV()] > dist[u] + i->getWeight())
+                dist[i->getV()] = dist[u] + i->getWeight();
+        }
+    }
+
+    // Print the calculated shortest distances
+    for (int i = 0; i < V; i++)
+        (dist[i] == INF)? cout << "INF ": cout << dist[i] << " ";
+}
+
+// Driver program to test above functions
+int main()
+{
+    // Create a graph given in the above diagram. 
+    // Here vertex numbers are 0, 1, 2, 3, 4, 5 with 
+    // following mappings: 0=r, 1=s, 2=t, 3=x, 4=y, 5=z
+    Graph g(6);
+    g.addEdge(0, 1, 5);
+    g.addEdge(0, 2, 3);
+    g.addEdge(1, 3, 6);
+    g.addEdge(1, 2, 2);
+    g.addEdge(2, 4, 4);
+    g.addEdge(2, 5, 2);
+    g.addEdge(2, 3, 7);
+    g.addEdge(3, 4, -1);
+    g.addEdge(4, 5, -2);
+
+    int s = 1;
+    cout << "Following are shortest distances from source " << s <<" : ";
+    g.shortestPath(s);
+
+    return 0;
+}
